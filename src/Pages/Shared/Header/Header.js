050eaeccccd3444d1a9c30f6560a44058656927e
Samiulahmedanik <84468942+SAANIK45@users.noreply.github.com>
2021-10-14 02:04:40 +0600

    Build Header

---
+++ src/Pages/Shared/Header/Header.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import cartIcon from "../../../Images/header/trolley.png";
+import logo from "../../../Images/logo.png";
+import './Header.css';
+
+const Header = () => {
+    return (
+        <div>
+            <div className="header" sticky="top">
+                <Container>
+                    <div className="header-main">
+                        <Row className="header-fixed">
+                            <Col xs={12} md={2}>
+                                <div className="logo">
+                                    <img src={logo} alt="" />
+                                </div>
+                            </Col>
+                            <Col xs={12} md={10}>
+                                <div className="main-menu">
+                                    <ul>
+                                        <li><Link to="/cart" className="cartIcon"><img src={cartIcon} alt="" /></Link></li>
+                                        <li><Link to="/login">Login</Link></li>
+                                        <li><Link to="/signup" className="SignUp">Sign up</Link></li>
+                                    </ul>
+                                </div>
+                            </Col>
+                        </Row>                    
+                    </div>
+                </Container>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
