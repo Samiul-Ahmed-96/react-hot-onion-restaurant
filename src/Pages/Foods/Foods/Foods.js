78722d28819a28d62dabbac76479e05d2126e391
Samiulahmedanik <84468942+SAANIK45@users.noreply.github.com>
2021-10-14 15:42:14 +0600

    Make foods item

--- src/Pages/Foods/Foods/Foods.js
+++ src/Pages/Foods/Foods/Foods.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
 import Breakfast from '../Breakfast/Breakfast';
+import Lunch from '../Lunch/Lunch';
 
 const Foods = () => {
     return (
         <Container>
             <h1 className="text-white my-5">Our Meals</h1>
             <Breakfast></Breakfast>
+            <Lunch></Lunch>
         </Container>
     );
 };
--- src/Pages/Foods/Lunch/Lunch.js
+++ src/Pages/Foods/Lunch/Lunch.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Row } from 'react-bootstrap';
+import SingleLunch from '../SingleLunch/SingleLunch';
 
 const Lunch = () => {
+    const [lunch,setLunch] = useState([])
+
+    useEffect(()=>{
+        fetch("./lunch.json")
+        .then(res => res.json())
+        .then(data => setLunch(data))
+    },[])
     return (
         <div>
-            
+        <Row>
+        {
+            lunch.map(lunchItem => <SingleLunch key={lunchItem.id} lunchItem={lunchItem}></SingleLunch>)
+        }
+        </Row>
         </div>
     );
 };
